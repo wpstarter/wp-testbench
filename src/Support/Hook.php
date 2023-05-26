8ff7d4c0531cf57387264f56e557a862f5db893b
as247 <as247@vui360.com>
2023-05-26 16:12:26 +0700

    Test lib

---
+++ src/Support/Hook.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace WsTestsLib\Support;
+
+class Hook
+{
+    public static function addFilter($hook_name, $callback, $priority = 10, $accepted_args = 1 ) {
+        global $wp_filter;
+
+        if ( function_exists( 'add_filter' ) ) {
+            add_filter( $hook_name, $callback, $priority, $accepted_args );
+        } else {
+            $idx = static::buildUniqueId( $hook_name, $callback, $priority );
+
+            $wp_filter[ $hook_name ][ $priority ][ $idx ] = array(
+                'function'      => $callback,
+                'accepted_args' => $accepted_args,
+            );
+        }
+
+        return true;
+    }
+    public static function buildUniqueId( $hook_name, $callback, $priority ) {
+        if ( is_string( $callback ) ) {
+            return $callback;
+        }
+
+        if ( is_object( $callback ) ) {
+            // Closures are currently implemented as objects.
+            $callback = array( $callback, '' );
+        } else {
+            $callback = (array) $callback;
+        }
+
+        if ( is_object( $callback[0] ) ) {
+            // Object class calling.
+            return spl_object_hash( $callback[0] ) . $callback[1];
+        } elseif ( is_string( $callback[0] ) ) {
+            // Static calling.
+            return $callback[0] . '::' . $callback[1];
+        }
+    }
+}
---
+++ src/Support/helpers.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Adds hooks before loading WP.
+ *
+ * @since UT (3.7.0)
+ *
+ * @see add_filter()
+ * @global WP_Hook[] $wp_filter A multidimensional array of all hooks and the callbacks hooked to them.
+ *
+ * @param string   $hook_name     The name of the filter to add the callback to.
+ * @param callable $callback      The callback to be run when the filter is applied.
+ * @param int      $priority      Optional. Used to specify the order in which the functions
+ *                                associated with a particular action are executed.
+ *                                Lower numbers correspond with earlier execution,
+ *                                and functions with the same priority are executed
+ *                                in the order in which they were added to the action. Default 10.
+ * @param int      $accepted_args Optional. The number of arguments the function accepts. Default 1.
+ * @return true Always returns true.
+ */
+function tests_add_filter( $hook_name, $callback, $priority = 10, $accepted_args = 1 ) {
+    return \WsTestsLib\Support\Hook::addFilter($hook_name,$callback,$priority,$accepted_args);
+}
