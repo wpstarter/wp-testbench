1e0bcadbfdabcbfa138f2ca9c2707d6a0bb8a45a
as247 <as247@vui360.com>
2023-06-04 22:14:53 +0700

    Fix path

--- includes/functions.php
+++ includes/functions.php
@@ -19,8 +19,8 @@ if(!function_exists('wp_testbench_putenv')) {
         putenv("$var=$val");
     }
 }
-if(!function_exists('wp_testbench_path_is_absolute')){
-    function wp_testbench_path_is_absolute( $path ) {
+if(!function_exists('_wp_testbench_path_is_absolute')){
+    function _wp_testbench_path_is_absolute( $path ) {
         /*
          * This is definitive if true but fails if $path does not exist or contains
          * a symbolic link.
@@ -42,6 +42,36 @@ if(!function_exists('wp_testbench_path_is_absolute')){
         return ( '/' === $path[0] || '\\' === $path[0] );
     }
 }
+if(!function_exists('_wp_testbench_clean_path')){
+    function _wp_testbench_clean_path($path) {
+        // Split the path into individual directory components
+        $components = explode('\/', $path);
+
+        // Initialize an empty array to store the cleaned components
+        $cleanedComponents = [];
+
+        // Iterate through each component
+        foreach ($components as $component) {
+            // Skip empty components and the current directory (".")
+            if ($component === '' || $component === '.') {
+                continue;
+            }
+
+            // If the component is the parent directory (".."), remove the last cleaned component
+            if ($component === '..') {
+                array_pop($cleanedComponents);
+            } else {
+                // Add the component to the cleaned components array
+                $cleanedComponents[] = $component;
+            }
+        }
+
+        // Reconstruct the cleaned path by joining the cleaned components with "/"
+        $cleanedPath = implode(DIRECTORY_SEPARATOR, $cleanedComponents);
+
+        return $cleanedPath;
+    }
+}
 if(!function_exists('wp_testbench_path')){
     /**
      * Get absolute path from working dir to current path
@@ -49,13 +79,13 @@ if(!function_exists('wp_testbench_path')){
      * @return string
      */
     function wp_testbench_path($path=''){
-        if(wp_testbench_path_is_absolute($path)){
+        if(_wp_testbench_path_is_absolute($path)){
             return $path;
         }
         $currentDir=wp_testbench_env('WP_TESTBENCH_WORKING_DIR');
         if(!$currentDir){
             $currentDir=getcwd();
         }
-        return realpath(rtrim($currentDir,'\/').DIRECTORY_SEPARATOR.$path);
+        return _wp_testbench_clean_path(rtrim($currentDir,'\/').DIRECTORY_SEPARATOR.$path);
     }
 }
